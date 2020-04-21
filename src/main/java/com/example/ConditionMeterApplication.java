700c702f1c84c945048e64890c1e944775fcb2f3
yosukeYamada <yamada_141@yahoo.co.jp>
2020-04-21 15:34:02 +0900

    first commit

---
+++ src/main/java/com/example/ConditionMeterApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionMeterApplication.class, args);
+	}
+
+}
